663863c881400ba3faee1c77de944ac5fe5c7f3a
menty44 <menty44@gmail.com>
2015-10-25 22:54:16 +0300

    created database called registration

---
+++ db/migrate/20151025195240_create_registrations.rb
@@ -0,0 +1,16 @@
+class CreateRegistrations < ActiveRecord::Migration
+  def change
+    create_table :registrations do |t|
+      t.string :firstname
+      t.string :lastname
+      t.string :email
+      t.string :gender
+      t.int :phonenumber
+      t.string :city
+      t.string :password
+      t.string :role
+
+      t.timestamps null: false
+    end
+  end
+end
